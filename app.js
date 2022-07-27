3395b32e4020ce4eca0cc32167acfa570e25a2d1
erickhangati <ekhangati@gmail.com>
2022-07-27 11:44:31 +0300

    Prepared app for development

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -80,6 +81,9 @@ app.use((req, res, next) => {
   // console.log(req.cookies);
   next();
 });
+
+// --- Compress text sent to client
+app.use(compression());
 
 // 2) ROUTE HANDLERS
 
